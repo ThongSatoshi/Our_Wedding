383bf8a0388f57737b37a8fc32d9789dfb11e82d
ThongSatoshi <ThongSatoshi>
2024-08-16 00:06:59 +0700

    Test git

--- Source Code/js/styling_and_animations.js
+++ Source Code/js/styling_and_animations.js
@@ -4,6 +4,7 @@ let navigationMenu = document.getElementById("navigationMenu");
 let headerContainer = document.getElementById("headerContainer");
 let siteLogo = document.getElementById("siteLogo");
 let siteName = document.getElementById("siteName");
+let shortClip = document.getElementById("shortClip");
 
 pageContent.onscroll = () => {
   shrinkNavigationMenu();
@@ -12,27 +13,27 @@ pageContent.onscroll = () => {
 
 // Shrink navigation menu and rearrange menu tabs when scrolls down past x amount of px
 function shrinkNavigationMenu() {
-  if (pageContent.scrollTop > 80) {
-    navigationMenu.style.width = "70vw";
-    navigationMenu.style.paddingLeft = "9vw";
-    navigationMenu.style.paddingTop = "0.25vh";
-    navigationMenu.style.paddingBottom = "0.25vh";
-    navigationMenu.style.borderBottomLeftRadius = "10px";
-    navigationMenu.style.borderBottomRightRadius = "10px";
-    navigationMenu.style.boxShadow = "8px 8px #ff7878";
-    navigationMenu.style.transform = "translateX(15vw)";
-    headerContainer.style.display = "none";
-  } else {
-    navigationMenu.style.width = "99vw";
-    navigationMenu.style.paddingLeft = "45vw";
-    navigationMenu.style.paddingTop = "1vh";
-    navigationMenu.style.paddingBottom = "1vh";
-    navigationMenu.style.borderRadius = "0";
-    navigationMenu.style.boxShadow = "none";
-    navigationMenu.style.transform = "translate(0)";
-    headerContainer.style.display = "block";
-  }
-}
+  // if (pageContent.scrollTop > 80) {
+  //   navigationMenu.style.minWidth = "70vw";
+  //   navigationMenu.style.paddingLeft = "9vw";
+  //   navigationMenu.style.paddingTop = "0.25vh";
+  //   navigationMenu.style.paddingBottom = "0.25vh";
+  //   navigationMenu.style.borderBottomLeftRadius = "10px";
+  //   navigationMenu.style.borderBottomRightRadius = "10px";
+  //   navigationMenu.style.boxShadow = "8px 8px #ff7878";
+  //   navigationMenu.style.transform = "translateX(15vw)";
+  //   headerContainer.style.display = "none";
+  // } else {
+  //   navigationMenu.style.minWidth = "99vw";
+  //   navigationMenu.style.paddingLeft = "45vw";
+  //   navigationMenu.style.paddingTop = "1vh";
+  //   navigationMenu.style.paddingBottom = "1vh";
+  //   navigationMenu.style.borderRadius = "0";
+  //   navigationMenu.style.boxShadow = "none";
+  //   navigationMenu.style.transform = "translate(0)";
+  //   headerContainer.style.display = "block";
+  // };
+};
 
 // Autoscroll animation for wedding photos
 let weddingPhotoIdArr = [];
@@ -40,16 +41,16 @@ for (i = 1; i < 11; i++) {
   var str = "weddingPhoto" + i;
   weddingPhotoIdArr.push(str);
   console.log(weddingPhotoIdArr);
-}
+};
 
 if (pageContent.style.width == "100%" && pageContent.style.height == "100%") {
   window.setInterval(autoScrollImg(false), 10000);
-}
+};
 
 function autoScrollImg(isStop) {
   if (isStop == false) {
     console.log((n += 1));
   } else {
     return;
-  }
-}
+  };
+};
